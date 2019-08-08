390495b7f3e92c1f4eb4ea013b8e4184147fe122
cgonzaru <cristina.glez.rubio@gmail.com>
2019-08-08 10:16:26 +0200

    add list in html

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -4,7 +4,7 @@ console.log('>> Ready :)');
 
 const input = document.querySelector('.input__field');
 const btn = document.querySelector('.btn');
-const seriesList = document.querySelector('.series__list');
+const showList = document.querySelector('.show__list');
 
 const url = 'http://api.tvmaze.com/search/shows?q=';
 let ENDPOINT = '';
@@ -21,7 +21,31 @@ function getSeries () {
   fetch(ENDPOINT)
     .then(response => response.json())
     .then(data => {
-      console.log(data[0].show.name);
+      //console.log(data[0].show.name);
+
+      //obtenemos el name para meterlo en la lista
+      let nameList = '';
+      for (let item of data) {
+        console.log(item.show.name);
+
+
+
+
+        nameList += `
+          <li class="show" data-id="${item.show.id}">
+            <div class="show__container">
+              <h2 class="title__name">${item.show.name}</h2>
+              <img src="${item.show.image.medium}" alt="${item.show.name}">
+            </div>
+          </li>
+        `;
+
+      }
+
+
+      showList.innerHTML = nameList;
+
+
     });
 }
 
