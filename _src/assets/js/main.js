4d0a8d728a5f7c554c8cc3a2dc40fa534b6d2737
cgonzaru <cristina.glez.rubio@gmail.com>
2019-08-08 11:07:58 +0200

    add img TV

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -23,28 +23,34 @@ function getSeries () {
     .then(data => {
       //console.log(data[0].show.name);
 
-      //obtenemos el name para meterlo en la lista
       let nameList = '';
       for (let item of data) {
         console.log(item.show.name);
 
-
-
-
-        nameList += `
-          <li class="show" data-id="${item.show.id}">
-            <div class="show__container">
-              <h2 class="title__name">${item.show.name}</h2>
-              <img src="${item.show.image.medium}" alt="${item.show.name}">
-            </div>
-          </li>
-        `;
+        if (item.show.image === null) {
+          console.log('no tenemos imagen');
+          nameList += `
+            <li class="show" data-id="${item.show.id}">
+              <div class="show__container">
+                <img src="https://via.placeholder.com/210x295/A7A7A7/?text=TV" alt="${item.show.name}">
+                <h2 class="title__name">${item.show.name}</h2>
+              </div>
+            </li>
+          `;
+        } else {
+          nameList += `
+            <li class="show" data-id="${item.show.id}">
+              <div class="show__container">
+                <img src="${item.show.image.medium}" alt="${item.show.name}">
+                <h2 class="title__name">${item.show.name}</h2>
+              </div>
+            </li>
+          `;
+        }
 
       }
 
-
       showList.innerHTML = nameList;
-
 
     });
 }
