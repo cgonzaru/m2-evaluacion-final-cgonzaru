68c13f5f9c1e2e791fe7fd67d9c660639381a7bf
cgonzaru <cristina.glez.rubio@gmail.com>
2019-08-08 09:08:03 +0200

    add field and button, add eventListener

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const input = document.querySelector('.input__field');
+const btn = document.querySelector('.btn');
+const seriesList = document.querySelector('.series__list');
+
+function writte () {
+  seriesList.innerHTML = input.value;
+}
+
+btn.addEventListener('click', writte);
